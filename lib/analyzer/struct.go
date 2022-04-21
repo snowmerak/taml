54c8ed790680de90a11ef3cb80af68c83f93e0fc
snowmerak <snowmerak@outlook.com>
2022-04-22 02:27:39 +0900

    make struct analyzer

---
+++ lib/analyzer/struct.go
@@ -0,0 +1,40 @@
+package analyzer
+
+import (
+	"errors"
+	"reflect"
+)
+
+type Member struct {
+	Name      string
+	Type      reflect.Type
+	SubMmbers []Member
+}
+
+func MembersOf(t reflect.Type) ([]Member, error) {
+	typ := t
+	if typ.Kind() != reflect.Struct {
+		return nil, errors.New("not a struct")
+	}
+	members := make([]Member, 0, typ.NumField())
+	for i := 0; i < typ.NumField(); i++ {
+		f := typ.Field(i)
+		if f.Type.Kind() == reflect.Struct {
+			subMembers, err := MembersOf(f.Type)
+			if err != nil {
+				return nil, err
+			}
+			members = append(members, Member{
+				Name:      f.Name,
+				Type:      f.Type,
+				SubMmbers: subMembers,
+			})
+		} else {
+			members = append(members, Member{
+				Name: f.Name,
+				Type: f.Type,
+			})
+		}
+	}
+	return members, nil
+}
