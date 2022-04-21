73496870ff3765499db0753dc6bf754b72c054f1
snowmerak <snowmerak@outlook.com>
2022-04-22 02:15:45 +0900

    move into lib

--- test/main.go
+++ test/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/snowmerak/tson/strcase"
+	"github.com/snowmerak/tson/lexer/jsvalues"
 )
 
 func main() {
-	s := "FooBar"
-	fmt.Println(strcase.PascalToSnake(s))
+	t := "{[{\"a\":1,\"b\":2},{\"a\":3,\"b\":4}, 1, 2, 3, true, null, [1, 2, 3, [1, 2, 3]]]}"
+	_, v, _, err := jsvalues.FindObject([]byte(t))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(v))
 }
