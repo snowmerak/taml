0a28bfaabaf68efb685d330060b8eef1227c9809
snowmerak <snowmerak@outlook.com>
2022-04-22 01:00:24 +0900

    make ws

---
+++ lexer/whitespace/ws.go
@@ -0,0 +1,22 @@
+package whitespace
+
+import "errors"
+
+func Find(data []byte) ([]byte, []byte, []byte, error) {
+	s := -1
+	e := -1
+	for i := 0; i < len(data); i++ {
+		if data[i] == ' ' || data[i] == '\t' || data[i] == '\n' || data[i] == '\r' {
+			if s == -1 {
+				s = i
+			}
+			e = i
+			continue
+		}
+		break
+	}
+	if s == e {
+		return nil, nil, nil, errors.New("whitespace not found")
+	}
+	return data[:s], data[s:e], data[e:], nil
+}
---
+++ lexer/whitespace/ws_test.go
@@ -0,0 +1,5 @@
+package ws_test
+
+import "testing"
+
+func TestWhiteSpace(t *testing.T) {}
